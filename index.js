808efadfb197f7325d16a41c11c37c0bdccf2eeb
Kalyani <kallu@Kalyanis.local>
2025-03-08 19:58:09 -0500

    checked-property

---
+++ index.js
@@ -0,0 +1,36 @@
+const myCheckBox=document.getElementById("myCheckBox");
+const visaBtn=document.getElementById("visaBtn");
+const masterCardBtn=document.getElementById("masterCardBtn");
+const payPalBtn=document.getElementById("payPalBtn");
+const mySubmit=document.getElementById("mySubmit");
+const subResult=document.getElementById("subResult");
+const paymentResult=document.getElementById("paymentResult");
+
+
+mySubmit.onclick= function(){
+
+    if(myCheckBox.checked){
+        subResult.textContent=`you are subscribed`;
+
+    }
+    else{
+        subResult.textContent="you are  not subscribed";
+
+    }
+    if(visaBtn.checked){
+        paymentResult.textContent="you are paying with visa"  ;
+      }
+    else if(masterCardBtn.checked){
+        paymentResult.textContent="you are paying with masterCard"  ;
+      }
+    else if(payPalBtn.checked){
+        paymentResult.textContent="you are paying with paypal"  ;
+      }
+    else{
+        paymentResult.textContent="you must select a payment type"  ;
+      }
+      
+    }     
+    
+      
+
